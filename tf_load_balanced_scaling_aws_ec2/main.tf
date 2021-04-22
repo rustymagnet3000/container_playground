a13613d887d5eecbc2486a8507c8de2db7aac66e
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-04-22 10:53:53 +0100

    Added lock example for TF resources

--- tf_load_balanced_scaling_aws_ec2/main.tf
+++ tf_load_balanced_scaling_aws_ec2/main.tf
@@ -1,85 +1,81 @@
 #https://github.com/gruntwork-io/intro-to-terraform
 
 provider "aws" {
-  region = "eu-west-2"
+  region  = "eu-west-2"
+  profile = "rm_terraform"
 }
 
 
 resource "aws_launch_configuration" "example" {
-    image_id           = "ami-0fbec3e0504ee1970"
-    instance_type = "t2.micro"
-    security_groups = [aws_security_group.instance.id]
+  image_id        = "ami-0fbec3e0504ee1970"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
-    user_data = <<-EOF
+  user_data = <<-EOF
             #!/bin/bash
             echo "Hello RM World" > index.html
             nohup busybox httpd -f -p "${var.server_port}" &
             EOF
-    lifecycle {
-        create_before_destroy = true
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # scale up
 resource "aws_autoscaling_group" "example" {
-    launch_configuration = aws_launch_configuration.example.id
-    availability_zones = data.aws_availability_zones.all.names
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = data.aws_availability_zones.all.names
 
   min_size = 2
   max_size = 10
 
-    load_balancers = [aws_elb.example.name]
-    health_check_type = "ELB"
+  load_balancers    = [aws_elb.example.name]
+  health_check_type = "ELB"
 
   tag {
-      key = "Name"
-      value = "terraform-asg-example"
-      propagate_at_launch = true
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
 # Add a classic load balancer ( elb == a clb template)
 resource "aws_elb" "example" {
-  name = "terraform-asg-example"
-  security_groups = [aws_security_group.elb.id]
+  name               = "terraform-asg-example"
+  security_groups    = [aws_security_group.elb.id]
   availability_zones = data.aws_availability_zones.all.names
 
-    health_check {
-        target = "HTTP:${var.server_port}/"
-        interval = 30
-        timeout = 3
-        healthy_threshold = 2
-        unhealthy_threshold = 2        
-    }
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 
-    listener {
-        lb_port = var.elb_port
-        lb_protocol = "http"
-        instance_port = var.server_port
-        instance_protocol = "http"
-    }
+  listener {
+    lb_port           = var.elb_port
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
 }
 
 # By default no ingress/egress traffic
 resource "aws_security_group" "elb" {
-    name = "terraform-example-elb"
+  name = "terraform-example-elb"
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port   = var.elb_port
-        to_port     = var.elb_port
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = var.elb_port
+    to_port     = var.elb_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-
-
-
-
-
---
+++ tf_state/main.tf
@@ -0,0 +1,43 @@
+#https://blog.gruntwork.io/how-to-manage-terraform-state-28f5697e68fa
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  # Name of bucket
+  bucket = "yd-terraform-up-and-running-state"
+  # Enable versioning so we can see the full revision history of our state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "yd-terraform-up-and-running-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
